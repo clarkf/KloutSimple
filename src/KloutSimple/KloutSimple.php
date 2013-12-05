343317ac3fb4835aac3ab281b9670edc0d6b50f8
Clark Fischer <clark.fischer@gmail.com>
2013-12-05 12:05:37 -0800

    quickadd

---
+++ src/KloutSimple/KloutSimple.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of KloutSimple.
+ *
+ * PHP Version 5+
+ *
+ * @category API
+ * @package  KloutSimple
+ * @author   Clark Fischer <clark.fischer@gmail.com>
+ * @license  MIT http://opensource.org/licenses/MIT
+ * @link     https://github.com/clarkf/KloutSimple
+ */
+
+namespace KloutSimple;
+
+/**
+ * The main KloutSimple class
+ *
+ * @category API
+ * @package  KloutSimple
+ * @author   Clark Fischer <clark.fischer@gmail.com>
+ * @license  MIT http://opensource.org/licenses/MIT
+ * @link     https://github.com/clarkf/KloutSimple
+ */
+class KloutSimple
+{
+}
---
+++ tests/KloutSimple/Test/KloutSimpleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace KloutSimple\Test;
+
+use \KloutSimple\KloutSimple;
+
+class KloutSimpleTest extends \PHPUnit_Framework_TestCase
+{
+    function testConstructor() {
+        $x = new KloutSimple();
+    }
+}
